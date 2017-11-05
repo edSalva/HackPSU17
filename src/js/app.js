83823b000abf592038b63c47d89500510bef5da9
Eddie Salvatierra <edsalva123@gmail.com>
2017-11-04 23:37:35 -0400

    Querying attempt

--- src/js/app.js
+++ src/js/app.js
@@ -9,5 +9,25 @@ function showSpeed(data) {
 									}
 };
 
+function getReccomend(position) {
+	console.log(position);
+
+	var lat = pos.coords.latitude;
+	var lon = pos.coords.longitude;
+
+	var oReq = new XMLHttpRequest();
+	oReq.addEventListener("load", 
+			function(resp) {
+		var recText = document.getElementById('rec_speed');
+		recText.innerHTML = this.responseText;
+			});
+
+	oReq.open("GET", url );//+ "/?lat=" + lot + "&lon=" + lon);
+	oReq.send();
+}
+
 gm.info.watchVehicleData(showSpeed, ['average_speed']);
 gm.info.getVehicleData(showSpeed, ['average_speed']);
+
+gm.info.watchPosition(getReccomend);
+gm.info.getCurrentPosition(getReccomend);
