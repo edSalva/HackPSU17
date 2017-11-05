9f0c4d1fa076923a810fb129ff146337197f0f4d
Eddie Salvatierra <edsalva123@gmail.com>
2017-11-04 20:41:10 -0400

    Practice Branching

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,13 @@
 // Your code goes here
 
-var vinElem = document.getElementById('vin');
-gm.info.getVehicleConfiguration(function(data) {
-  vinElem.innerHTML = gm.info.getVIN();
-});
+function showSpeed(data) {
+	  console.log(data);
+		  var speed = data.average_speed;
+			  if (speed !== undefined) {
+					    var speedText = document.getElementById('speed');
+							    speedText.innerHTML = speed;
+									}
+};
+
+gm.info.watchVehicleData(showSpeed, ['average_speed']);
+gm.info.getVehicleData(showSpeed, ['average_speed']);
