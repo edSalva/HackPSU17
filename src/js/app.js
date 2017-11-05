3c7aeedc7da682bb64d8a2f02ea2c90a6fb22d01
Ali Manan <alimanan1525@gmail.com>
2017-11-05 12:14:11 -0500

    initial UI

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,8 @@ var vinElem = document.getElementById('vin');
 gm.info.getVehicleConfiguration(function(data) {
   vinElem.innerHTML = gm.info.getVIN();
 });
+
+var CurrentSpeedElem = document.getElementById('CurrentSpeed');
+gm.info.getVehicleConfiguration(function(data) {
+  CurrentSpeedElem.innerHTML = gm.system.getSpeed();
+});
